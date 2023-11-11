ff5650e5486b764f14cdc9edc46e237034ab33f2
Sarah Keith <89654821+SarahKeithp@users.noreply.github.com>
2023-11-11 18:37:52 -0300

    Update analysis-bar.py
    Change the comments in analysis-bar to english

--- analytics/descriptive/analysis-bar.py
+++ analytics/descriptive/analysis-bar.py
@@ -2,14 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sqlite3
 
-# Conecta ao banco de dados SQLite
+# Connect to database
 conn = sqlite3.connect("sorveteria.db")
 
-# importa as bases
+# select the dataframe
 marketing = pd.read_sql("SELECT * FROM marketing", conn)
 df = pd.DataFrame(marketing)
 
-# Cria o gráfico de barras dos investimentos em diferentes meios de campanha
+# Create the graph
 plt.figure(figsize=(10, 6))
 plt.bar(df['meio'], df['investimento'])
 plt.xlabel('Meio de Campanha')
@@ -18,7 +18,7 @@ plt.title('Investimento em Campanhas de Marketing por Meio')
 plt.xticks(rotation=45)
 plt.tight_layout()
 
-# Salva a imagem no diretório de destino
+# Salve the image
 file_name = 'plt_analysis_bar.png'
 diretorio_destino = 'plots/descriptive/'
 plt.savefig(f'{diretorio_destino}{file_name}')
